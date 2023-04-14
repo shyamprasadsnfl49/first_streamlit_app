a6b1c10d0e9970246d024447b07416d585137244
shyamprasadsnfl49 <130452007+shyamprasadsnfl49@users.noreply.github.com>
2023-04-14 19:22:38 +0530

    Update streamlit_app.py
    fix urllib typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from ulib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
